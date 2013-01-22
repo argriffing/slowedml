357e2ab10371619d835a0a143a18c59eed05cd78
alex <argriffi@ncsu.edu>
2013-01-22 13:24:59 -0500

    more debugging of the test infrastructure

--- slowedml/tests/__init__.py
+++ slowedml/tests/__init__.py
@@ -1,3 +1,10 @@
 """
 Perhaps nosetests requires this file to exist in this directory.
 """
+
+#FIXME: is there a better way to automatically import
+#FIXME: all of the python test files?
+
+import test_design
+import test_phylip
+
